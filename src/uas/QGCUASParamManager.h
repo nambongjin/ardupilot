69b176a71a68ef08613991f79732c25dcaf9f2fc
nambongjin <40817106+nambongjin@users.noreply.github.com>
2018-09-07 13:20:13 +0900

    src\uas\QGCUASParamManager.h UPDATE01

--- src/uas/QGCUASParamManager.h
+++ src/uas/QGCUASParamManager.h
@@ -37,8 +37,10 @@ public:
     virtual bool loadParamsFromFile(const QString &filename,ParamFileType type) = 0;
 
     /** @brief Request an update for the parameter list */
+    /** brief 매개 변수 목록에 대한 업데이트 요청 */
     void requestParameterListUpdate(int component = 0);
     /** @brief Request an update for this specific parameter */
+    /** @ brief이 특정 매개 변수에 대한 업데이트 요청 */
     virtual void requestParameterUpdate(int component, const QString& parameter) = 0;
 
 signals:
@@ -48,25 +50,27 @@ signals:
 
 public slots:
     /** @brief Write one parameter to the MAV */
+    /* * @ brief MAV에 하나의 매개 변수 쓰기 */
     virtual void setParameter(int component, QString parameterName, QVariant value) = 0;
     /** @brief Request list of parameters from MAV */
+    /* * brief MAV로부터 매개 변수 목록 요청 */
     virtual void requestParameterList() = 0;
 
 protected:
-    UASInterface* mav;   ///< The MAV this widget is controlling
-    QMap<int, QMap<QString, QVariant>* > changedValues; ///< Changed values
-    QMap<int, QMap<QString, QVariant>* > parameters; ///< All parameters
-    QVector<bool> received; ///< Successfully received parameters
-    QMap<int, QList<int>* > transmissionMissingPackets; ///< Missing packets
-    QMap<int, QMap<QString, QVariant>* > transmissionMissingWriteAckPackets; ///< Missing write ACK packets
-    bool transmissionListMode;       ///< Currently requesting list
-    QMap<int, bool> transmissionListSizeKnown;  ///< List size initialized?
-    bool transmissionActive;         ///< Missing packets, working on list?
-    quint64 transmissionTimeout;     ///< Timeout
-    QTimer retransmissionTimer;      ///< Timer handling parameter retransmission
-    int retransmissionTimeout; ///< Retransmission request timeout, in milliseconds
-    int rewriteTimeout; ///< Write request timeout, in milliseconds
-    int retransmissionBurstRequestSize; ///< Number of packets requested for retransmission per burst
+    UASInterface* mav;   ///< The MAV this widget is controlling// / <이 위젯이 제어하는 ​​MAV
+    QMap<int, QMap<QString, QVariant>* > changedValues; ///< Changed values// / <변경된 값
+    QMap<int, QMap<QString, QVariant>* > parameters; ///< All parameters// / <모든 매개 변수
+    QVector<bool> received; ///< Successfully received parameters// / <매개 변수를 받았습니다.
+    QMap<int, QList<int>* > transmissionMissingPackets; ///< Missing packets// / <누락 된 패킷
+    QMap<int, QMap<QString, QVariant>* > transmissionMissingWriteAckPackets; ///< Missing write ACK packets// / <쓰기 ACK 패킷 누락
+    bool transmissionListMode;       ///< Currently requesting list// / <현재 요청 목록
+    QMap<int, bool> transmissionListSizeKnown;  ///< List size initialized?// / <리스트 크기가 초기화 되었습니까?
+    bool transmissionActive;         ///< Missing packets, working on list?// / <빠진 패킷,리스트 작업중?
+    quint64 transmissionTimeout;     ///< Timeout// / <Timeout
+    QTimer retransmissionTimer;      ///< Timer handling parameter retransmission// / <타이머 처리 매개 변수 재전송
+    int retransmissionTimeout; ///< Retransmission request timeout, in milliseconds// / <재전송 요청 시간 제한 (밀리 초 단위)
+    int rewriteTimeout; ///< Write request timeout, in milliseconds// / <쓰기 요청 제한 시간 (밀리 초)
+    int retransmissionBurstRequestSize; ///< Number of packets requested for retransmission per burst// / <버스트 당 재전송 요청 된 패킷 수
 
 };
 
