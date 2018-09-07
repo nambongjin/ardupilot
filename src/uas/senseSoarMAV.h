dae9e269dce079f77e24fe7dea737d93416b1321
sangho89 <cainen1@naver.com>
2018-09-07 16:06:44 +0900

    src\uas\senseSoarMAV.h

--- src/uas/senseSoarMAV.h
+++ src/uas/senseSoarMAV.h
@@ -18,10 +18,11 @@ public:
 	~senseSoarMAV(void);
 public slots:
     /** @brief Receive a MAVLink message from this MAV */
+    /** brief이 MAV로부터 MAVLink 메시지 받기 */
     void receiveMessage(LinkInterface* link, mavlink_message_t message);
 protected:
-	float m_rotVel[3]; // Rotational velocity in the body frame
-	uint8_t senseSoarState;
+	float m_rotVel[3];      // Rotational velocity in the body frame
+	uint8_t senseSoarState; // 본체 프레임의 회전 속도
 private:
 	void quat2euler(const double *quat, double &roll, double &pitch, double &yaw); 
 };
