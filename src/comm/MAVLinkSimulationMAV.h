b40ae755bd0869f259463767129eda365f1cc976
sangho89 <cainen1@naver.com>
2018-09-07 15:53:32 +0900

    src\comm\MAVLinkSimulationMAV.h

--- src/comm/MAVLinkSimulationMAV.h
+++ src/comm/MAVLinkSimulationMAV.h
@@ -70,6 +70,20 @@ protected:
 //        return mavlink_finalize_message(msg, system_id, component_id, i);
 //    }
 
+    // FIXME MAVLINKV10PORTINGNEEDED
+//     정적 인라인 uint16_t mavlink_msg_heartbeat_pack_version_free (uint8_t system_id, uint8_t component_id, mavlink_message_t * msg, uint8_t 유형, uint8_t 자동 조종 장치, uint8_t 버전) {
+//         uint16_t i = 0;
+//         msg-> msgid = MAVLINK_MSG_ID_HEARTBEAT;
+
+//         i + = put_uint8_t_by_index (type, i, msg-> payload); // MAV 유형 (MAV_TYPE ENUM에 정의 된 최대 15 가지 유형의 quadrotor, 헬리콥터 등)
+//         i + = put_uint8_t_by_index (자동 조종 장치, i, msg-> payload); // MAV_AUTOPILOT_TYPE ENUM에 정의 된 자동 파일럿 유형 : 0 : 일반, 1 : PIXHAWK, 2 : SLUGS, 3 : Ardupilot (최대 15 개 유형)
+//         i + = put_uint8_t_by_index (version, i, msg-> payload); // MAVLink 버전
+
+//         mavlink_finalize_message (msg, system_id, component_id, i)를 반환합니다.
+//     }
+
+
+
 };
 
 #endif // MAVLINKSIMULATIONMAV_H
