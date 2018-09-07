2144c55ae163d49885843fea27523961b4c111a3
nambongjin <40817106+nambongjin@users.noreply.github.com>
2018-09-07 10:20:49 +0900

    libs\thirdParty\quazip\quaadler32.h UPDATE01

--- libs/thirdParty/quazip/quaadler32.h
+++ libs/thirdParty/quazip/quaadler32.h
@@ -10,6 +10,11 @@
  * This class wrappers the adler32 function with the QuaChecksum32 interface.
  * See QuaChecksum32 for more info.
  */
+// / Adler32 체크섬
+/* * \ class QuaAdler32 quaadler32.h <quazip / quaadler32.h>
+ *이 클래스는 QuadChecksum32 인터페이스로 adler32 함수를 래퍼합니다.
+ * 자세한 정보는 QuaChecksum32를 참조하십시오.
+ */
 class QUAZIP_EXPORT QuaAdler32 : public QuaChecksum32
 {
 
