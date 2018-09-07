bdf721a2eeb79c84cda562a33db4c9cae316380c
sangho89 <cainen1@naver.com>
2018-09-07 15:55:49 +0900

    src\output\kmlcreator.h

--- src/output/kmlcreator.h
+++ src/output/kmlcreator.h
@@ -18,6 +18,8 @@ extern mavlink_status_t m_mavlink_status[MAVLINK_COMM_NUM_BUFFERS]; // defined i
 
 // these definitions should match those in ardupilot/libraries/AP_GPS/AP_GPS.h
 // the number of GPS leap seconds
+// 이 정의는 ardupilot / libraries / AP_GPS / AP_GPS.h의 정의와 일치해야합니다.
+// GPS 윤초 초 수
 #define GPS_LEAPSECONDS 18ULL
 #define UNIX_OFFSET_SEC (315964800ULL - GPS_LEAPSECONDS)
 #define SEC_PER_WEEK    (7ULL * 86400ULL)
@@ -29,6 +31,9 @@ namespace kml {
 /**
  * @brief A GPS record from a log file.
  */
+/**
+ * @brief 로그 파일의 GPS 기록입니다.
+ */
 struct GPSRecord: DataLine {
     QString hdop()  { return values.value("HDop"); }
     QString lat()   { return values.value("Lat"); }
@@ -39,6 +44,7 @@ struct GPSRecord: DataLine {
     QString vz()    { return values.value("VZ"); }
 
     // older logs have TimeMS instead of TimeUS; Also GMS->GPSTimeMS and GWk->Week
+    // 이전 로그에는 TimeUS 대신 TimeMS가 있습니다. 또한 GMS-> GPSTimeMS 및 GWk-> 주
     QString msec()  {
         if (values.contains("GMS")) {
             return values.value("GMS");
@@ -79,19 +85,24 @@ struct GPSRecord: DataLine {
 
     qint64 getUtc_ms() {
         // msec since start of week (max value is 2^29.17, so it just fits in a signed 32 bit int)
+        // msec (최대 값은 2 ^ 29.17이므로 서명 된 32 비트 int에 맞음)
         int32_t week_ms = this->msec().toInt();
         // weeks since 6 Jan 1980
+        // 1980 년 1 월 6 일 이후 주
         int32_t week = this->week().toInt();
 
         // this is the offset as defined in ardupilot/libraries/AP_GPS/AP_GPS.h
+        // ardupilot / libraries / AP_GPS / AP_GPS.h에 정의 된 오프셋입니다.
         qint64 utc_msec = UNIX_OFFSET_SEC * 1000LL + week * SEC_PER_WEEK * 1000LL + week_ms;
         return utc_msec;
     }
 
     void setUtCTime(qint64 timeUS) {
         // convert to msec
+        // msec로 변환
         qint64 utc = timeUS / 1000LL;
         // offset to GPS epoch
+        // GPS 신기원으로 오프셋
         utc = utc - (UNIX_OFFSET_SEC * 1000LL);
 
         int32_t week = utc / (SEC_PER_WEEK * 1000LL);
@@ -109,6 +120,9 @@ struct GPSRecord: DataLine {
 
 /**
  * @brief A MODE record from a log file.
+ */
+/**
+ * @brief 로그 파일의 MODE 레코드.
  */
 struct ModeRecord: DataLine {
     QString modeNum()   {
@@ -139,6 +153,9 @@ struct ModeRecord: DataLine {
 /**
  * @brief A POS record from a log file.
  */
+/**
+ * @brief 로그 파일의 POS 레코드.
+ */
 struct POSRecord: DataLine {
     QString lat()  { return values.value("Lat"); }
     QString lng()    { return values.value("Lng"); }
@@ -158,6 +175,9 @@ struct POSRecord: DataLine {
 
 /**
  * @brief An ATT record from a log file.
+ */
+/**
+ * @brief 로그 파일의 ATT 레코드.
  */
 struct Attitude: DataLine {
     QString rollIn()  { return values.value("RollIn").isEmpty() ? values.value("DesRoll") : values.value("RollIn"); }
@@ -180,6 +200,9 @@ struct Attitude: DataLine {
 /**
  * @brief An AHR2 record from a log file.
  */
+/**
+ * @brief 로그 파일의 AHR2 레코드.
+ */
 struct AHR2: DataLine {
     QString roll()    { return values.value("Roll"); }
     QString pitch()   { return values.value("Pitch"); }
@@ -200,6 +223,10 @@ struct AHR2: DataLine {
 /**
  * @brief An XKQ1 record from a log file.
  */
+/**
+ * @brief 로그 파일의 XKQ1 레코드.
+ */
+
 struct XKQ1: DataLine {
 public:
     float q1, q2, q3, q4;
@@ -223,6 +250,9 @@ public:
 
 /**
  * @brief An NKQ1 record from a log file.
+ */
+/**
+ * @brief 로그 파일의 NKQ1 레코드.
  */
 struct NKQ1: DataLine {
 public:
@@ -249,6 +279,10 @@ public:
  * @brief A CMD line from a log file. Only used in this context to provide lat/lng/altitude
  * for generating waypoint data in the KML file.
  */
+/**
+ * @brief 로그 파일의 CMD 행. 이 문맥에서만 위도 / 경도 / 고도를 제공하는 데 사용됩니다.
+ * KML 파일에서 웨이 포인트 데이터를 생성합니다.
+ */
 struct CommandedWaypoint: DataLine {
 
     int index() { return values.value("CNum").toInt(); }
@@ -272,7 +306,7 @@ struct CommandedWaypoint: DataLine {
 
             if ((lat() == "0") || (lng() == "0")) {
                 return false; // Lat/Lng 0.0,0.0 is invalid
-            }
+            }                 // 위도 / 경도 0.0,0.0이 잘못되었습니다.
             return true;
 
         } else {
@@ -287,6 +321,9 @@ struct CommandedWaypoint: DataLine {
 
 /**
  * @brief A container of data for creating Placemarks in a KML file.
+ */
+/**
+ * @brief KML 파일에서 위치 표시를 만들기위한 데이터 컨테이너입니다.
  */
 struct Placemark {
     QString title;
@@ -338,6 +375,18 @@ struct SummaryData {
  * will be included in the .kmz file in that case. If you specify false for creating the .kmz file, the
  * block_plane_0.dae file will be left in the same directory as the .kml file.
  */
+/**
+ * @brief KML 파일을 생성하기위한 인터페이스.
+ *
+ * 그것을 사용하려면, 생성하고자하는 파일명으로 start ()를 호출하십시오.
+ *
+ 직렬 포트 또는 파일에서 dataflash 로그를 통해 line-by-line을 읽는 동안 processLine ()을 호출하십시오.
+* 각 줄. KML 제작자는 위치 표시 및 기타 항목을 수집합니다. 완료되면 finish () 및
+* kml 대신 .kmz 파일을 만들지 여부를 선택적으로 지정합니다. true를 전달하면 .kmz가 생성됩니다.
+* 파일을 생성하면 생성 된 KML 파일과 모델 파일이 포함 된 압축 된 .kmz 파일이 생성됩니다. block_plane_0.dae
+*는이 경우 .kmz 파일에 포함됩니다. .kmz 파일을 만들 때 false를 지정하면
+ * block_plane_0.dae 파일은 .kml 파일과 동일한 디렉토리에 남아 있습니다.
+ */
 class KMLCreator {
 public:
     void start(QString &fn);
@@ -369,6 +418,7 @@ private:
     SummaryData* m_summary;
 
     // save one Attitude and one quaternion record between GPS records
+    // GPS 기록 사이에 태도와 1 쿼터니온 기록을 저장합니다.
     XKQ1 m_xkq1;
     NKQ1 m_nkq1;
     AHR2 m_ahr2;
@@ -384,5 +434,6 @@ private:
 };
 
 } // namespace kml
+  // 네임 스페이스 kml
 
 #endif // KMLCREATOR_H
