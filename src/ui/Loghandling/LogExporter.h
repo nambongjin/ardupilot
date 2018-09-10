07afe2951b67e5b6a893595b269152779e2847d7
hwiya <hwijin0209@naver.com>
2018-09-10 10:57:13 +0900

    UPDATE 1
    컴파일 테스트 확인 완료

--- src/ui/Loghandling/LogExporter.h
+++ src/ui/Loghandling/LogExporter.h
@@ -23,7 +23,7 @@ This file is part of the APM_PLANNER project
  * @file LogExporter.h
  * @author Arne Wischmann <wischmann-a@gmx.de>
  * @date 28 Jan 2017
- * @brief File providing header for the log exporter classes
+ * @brief File providing header for the log exporter classes	 로그 내보내기 클래스 헤더 제공
  */
 
 
@@ -40,12 +40,19 @@ This file is part of the APM_PLANNER project
  *        the exporting workflow for every line oriented export.
  *        It provides a progress dialog with a cancel button.
  */
+
+/*
+* @brief LogExporterBase 클래스 - 다른 로그 내보내기에 사용됩니다. 핸들
+ * 모든 라인 지향 수출을위한 수출 워크 플로우.
+ * 취소 버튼이있는 진행 대화 상자를 제공합니다.
+*/
+
 class LogExporterBase
 {
 public:
 
     /**
-     * @brief Shared pointer for LogExporterBase objects
+     * @brief Shared pointer for LogExporterBase objects	  객체를위한 공유 포인터
      */
     typedef QSharedPointer<LogExporterBase> Ptr;
 
@@ -53,6 +60,12 @@ public:
      * @brief LogExporterBase - CTOR
      * @param parent - Parent widget needed for progress and info windows.
      */
+
+/*
+     * @brief LogExporterBase - CTOR
+     * @param parent - 진행 및 정보창에 필요한 부모 위젯.
+*/
+
     explicit LogExporterBase(QWidget *parent);
 
     /**
@@ -67,15 +80,24 @@ public:
      * @param dataStoragePtr - shared pointer to a filled LogdataStorage
      * @return QString with information about the export. Can be shown to the user.
      */
+
+/*
+     * @ brief exportToFile -에 의해 지적 된 LogdataStorage의 내용을 내 보냅니다.
+     * dataStoragePtr을 fileName이라는 이름의 파일에 추가하십시오.
+     * @param fileName - export의 파일명
+     * @param dataStoragePtr - 채워진 LogdataStorage에 대한 공유 포인터
+     * @return 내보내기에 대한 정보가있는 QString. 사용자에게 보여 질 수있다.
+*/
+
     QString exportToFile(const QString &fileName, LogdataStorage::Ptr dataStoragePtr);
 
 protected:
 
-    QString m_ExportResult; /// String containing the result of the export
+    QString m_ExportResult; /// String containing the result of the export	 내보내기 결과를 포함하는 문자열
 
 private:
 
-    QWidget *mp_parent; /// pointer to parent widget - do not delete
+    QWidget *mp_parent; /// pointer to parent widget - do not delete	 부모 위젯에 대한 포인터 - 삭제하지 않음
 
     /**
      * @brief startExport - must be implemented by derived classes. It has to setup
@@ -83,6 +105,14 @@ private:
      * @param fileName - filename for the export
      * @return true on success, false otherwise
      */
+
+/*
+     * @brief startExport - 파생 클래스에 의해 구현되어야합니다. 설치가 필요하다.
+     * 나중에 서면으로 전화 할 때 필요한 모든 전제 조건.
+     * @param fileName - export의 파일명
+     성공했을 경우는 true, 그렇지 않은 경우는 false
+*/
+
     virtual bool startExport(const QString &fileName) = 0;
 
     /**
@@ -90,6 +120,13 @@ private:
      *        export function for every logline stored in datamodel.
      * @param line - All data as a string
      */
+
+/*
+     * @brief writeLine - 파생 클래스에 의해 구현되어야합니다. 에 의해 호출됩니다.
+     * 데이터 모델에 저장된 모든 로그 라인에 대한 내보내기 기능.
+     * @param line - 모든 데이터를 문자열로
+*/
+
     virtual void writeLine(QString &line) = 0;
 
     /**
@@ -97,26 +134,33 @@ private:
      *        export function at the end of the export process. It should close all used
      *        resources.
      */
+
+/*
+     endExport - 파생 클래스에 의해 구현되어야합니다. 에 의해 호출됩니다.
+     * 내보내기 프로세스가 끝날 때 내보내기 기능. 사용 된 모든 것을 닫아야합니다.
+     * 자원.
+*/
+
     virtual void endExport() = 0;
 };
 
 //***********************************************************************
 
 /**
- * @brief The AsciiLogExporter class is used to export a *.log file.
+ * @brief The AsciiLogExporter class is used to export a *.log file.	  * @brief AsciiLogExporter 클래스는 * .log 파일을 내보내는 데 사용됩니다.
  */
 class AsciiLogExporter : public LogExporterBase
 {
 public:
 
     /**
-     * @brief Shared pointer for AsciiLogExporter objects
+     * @brief Shared pointer for AsciiLogExporter objects	      * @brief AsciiLogExporter 객체를위한 공유 포인터
      */
     typedef QSharedPointer<AsciiLogExporter> Ptr;
 
     /**
      * @brief AsciiLogExporter - CTOR
-     * @param parent - Parent widget needed for progress and info windows.
+     * @param parent - Parent widget needed for progress and info windows.	      * @param parent - 진행 및 정보창에 필요한 부모 위젯.
      */
     AsciiLogExporter(QWidget *parent);
 
@@ -127,23 +171,36 @@ public:
 
 private:
 
-    QFile m_outputFile;     /// file object for exporting
+    QFile m_outputFile;     /// file object for exporting	  내보내기를위한 파일 객체
 
     /**
      * @brief startExport - Creates and opens the output file
      * @param fileName - file name
      * @return - true on success, false otherwise
      */
+
+/*
+     * @brief startExport - 출력 파일을 생성하고 엽니 다.
+     * @param fileName - 파일명
+     * @return - 성공했을 경우는 true, 그렇지 않은 경우는 false
+*/
+
     virtual bool startExport(const QString &fileName);
 
     /**
      * @brief writeLine - writes the line directly into the output file.
      * @param line - string to be written to the file
      */
+
+/*
+     * @brief writeLine - 출력 파일에 직접 줄을 씁니다.
+     * @param line - 파일에 기입 해지는 캐릭터 라인
+*/
+
     virtual void writeLine(QString &line);
 
     /**
-     * @brief endExport - closes the output file
+     * @brief endExport - closes the output file	 endExport - 출력 파일을 닫습니다.
      */
     virtual void endExport();
 };
@@ -154,12 +211,18 @@ private:
  * @brief The KmlLogExporter class is used to export kml files which can be used
  *        with google earth.
  */
+
+/*
+ * @brief KmlLogExporter 클래스는 사용할 수있는 kml 파일을 내보내는 데 사용됩니다.
+ Google 어스와 함께 *.
+*/
+
 class KmlLogExporter : public LogExporterBase
 {
 public:
 
     /**
-     * @brief Shared pointer for KmlLogExporter objects
+     * @brief Shared pointer for KmlLogExporter objects	 KmlLogExporter 객체를위한 공유 포인터
      */
     typedef QSharedPointer<KmlLogExporter> Ptr;
 
@@ -167,6 +230,12 @@ public:
      * @brief KmlLogExporter - CTOR
      * @param parent - Parent widget needed for progress and info windows.
      */
+
+/*
+     * @brief KmlLogExporter - CTOR
+     * @param parent - 진행 및 정보창에 필요한 부모 위젯.
+*/
+
     KmlLogExporter(QWidget *parent, MAV_TYPE mav_type, double iconInterval);
 
     /**
@@ -176,13 +245,19 @@ public:
 
 private:
 
-    kml::KMLCreator m_kmlExporter;      /// KML export object
+    kml::KMLCreator m_kmlExporter;      /// KML export object	 KML 내보내기 개체
 
     /**
      * @brief startExport - sets up the kmlExporter.
      * @param fileName - file name to be used by the kmlExporter
      * @return
      */
+
+/*
+     * @brief startExport - kmlExporter를 설정합니다.
+     * @param fileName - kmlExporter에서 사용할 파일 이름입니다.
+*/
+
     virtual bool startExport(const QString &fileName);
 
     /**
@@ -190,12 +265,25 @@ private:
      *        neede data.
      * @param line - data to be analyzed
      */
+
+/*
+     * @brief writeLine - kmlExporter에 줄을 지정합니다.
+     * 데이터가 필요합니다.
+     * @param line - 분석 할 데이터
+*/
+
     virtual void writeLine(QString &line);
 
     /**
      * @brief endExport - exports the data collected by the kmlExporter to
      *        the file.
      */
+
+/*
+     * @brief endExport - kmlExporter에서 수집 한 데이터를 다음 위치로 내 보냅니다.
+     * 파일.
+*/
+
     virtual void endExport();
 };
 
